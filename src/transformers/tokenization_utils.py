cad86ea99bc042d0384388cc6dd3ac29358e2283
maor <maorivg@gmail.com>
2020-12-11 13:33:50 +0200

    added the temporary error to tokenization_utils to warn myself if the tokenization uses the wrong tokenization path

--- src/transformers/tokenization_utils.py
+++ src/transformers/tokenization_utils.py
@@ -413,15 +413,17 @@ class PreTrainedTokenizer(PreTrainedTokenizerBase):
                 tokens = self.tokenize(text, **kwargs)
                 return self.convert_tokens_to_ids(tokens)
             elif isinstance(text, (list, tuple)) and len(text) > 0 and isinstance(text[0], str):
-                if is_split_into_words:
-                    tokens = list(
-                        itertools.chain(*(self.tokenize(t, is_split_into_words=True, **kwargs) for t in text))
-                    )
-                    return self.convert_tokens_to_ids(tokens)
-                else:
-                    return self.convert_tokens_to_ids(text)
+                raise RuntimeError('This is not what you want!! you will get many UNKs!! - TODO - remove this warning')
+                # if is_split_into_words:
+                #     tokens = list(
+                #         itertools.chain(*(self.tokenize(t, is_split_into_words=True, **kwargs) for t in text))
+                #     )
+                #     return self.convert_tokens_to_ids(tokens)
+                # else:
+                #     return self.convert_tokens_to_ids(text)
             elif isinstance(text, (list, tuple)) and len(text) > 0 and isinstance(text[0], int):
-                return text
+                raise RuntimeError('This is not what you want!! you will get many UNKs!! - TODO - remove this warning')
+                # return text
             else:
                 if is_split_into_words:
                     raise ValueError(
@@ -495,15 +497,17 @@ class PreTrainedTokenizer(PreTrainedTokenizerBase):
                 tokens = self.tokenize(text, **kwargs)
                 return self.convert_tokens_to_ids(tokens)
             elif isinstance(text, (list, tuple)) and len(text) > 0 and isinstance(text[0], str):
-                if is_split_into_words:
-                    tokens = list(
-                        itertools.chain(*(self.tokenize(t, is_split_into_words=True, **kwargs) for t in text))
-                    )
-                    return self.convert_tokens_to_ids(tokens)
-                else:
-                    return self.convert_tokens_to_ids(text)
+                raise RuntimeError('This is not what you want!! you will get many UNKs!! - TODO - remove this warning')
+                # if is_split_into_words:
+                #     tokens = list(
+                #         itertools.chain(*(self.tokenize(t, is_split_into_words=True, **kwargs) for t in text))
+                #     )
+                #     return self.convert_tokens_to_ids(tokens)
+                # else:
+                #     return self.convert_tokens_to_ids(text)
             elif isinstance(text, (list, tuple)) and len(text) > 0 and isinstance(text[0], int):
-                return text
+                raise RuntimeError('This is not what you want!! you will get many UNKs!! - TODO - remove this warning')
+                # return text
             else:
                 raise ValueError(
                     "Input is not valid. Should be a string, a list/tuple of strings or a list/tuple of integers."
